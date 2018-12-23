cd54de851b1a0f6b6485e00f0d211eefd20faed3
tianyu <tianyu.hpu@gmail.com>
2018-12-23 22:07:18 +0800

    fix can't load spring.datasource.url

--- src/main/java/com/roncoo/eshop/inventory/Application.java
+++ src/main/java/com/roncoo/eshop/inventory/Application.java
@@ -9,6 +9,7 @@ import org.springframework.beans.factory.annotation.Value;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.EnableAutoConfiguration;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.context.properties.ConfigurationProperties;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.ComponentScan;
 import org.springframework.context.annotation.PropertySource;
@@ -25,19 +26,20 @@ import java.util.HashSet;
 import java.util.Set;
 
 @Data
+@Component
+@MapperScan("com.roncoo.eshop.inventory.mapper")
+@ComponentScan(basePackages="com.roncoo.eshop")
+@PropertySource(value={"classpath:application.properties"})
+@ConfigurationProperties(prefix = "spring.datasource")
 @SpringBootApplication
 @EnableAutoConfiguration
-@ComponentScan("com.roncoo.eshop")
-@Component
-@PropertySource(value={"classpath:application.properties"})
-@MapperScan("com.roncoo.eshop.inventory.mapper")
 public class Application {
 
-    @Value("${jdbcurl}")
+    @Value("url")
     private String url;
-    @Value("${username}")
+    @Value("username")
     private String username;
-    @Value("${password}")
+    @Value("password")
     private String password;
 
 	public static void main(String[] args) {
@@ -50,11 +52,11 @@ public class Application {
      */
 	@Bean
     public DataSource dataSource() {
-	    DruidDataSource dataSource =  new DruidDataSource();
+        DruidDataSource dataSource = new DruidDataSource();
         dataSource.setUrl(url);
         dataSource.setUsername(username);
         dataSource.setPassword(password);
-	    return dataSource;
+        return dataSource;
     }
 
     /**
@@ -82,12 +84,12 @@ public class Application {
     @Bean
     public JedisCluster jedisClusterFactory() {
         Set<HostAndPort> jedisClusterNodes = new HashSet<HostAndPort>();
-        jedisClusterNodes.add(new HostAndPort("192.168.0.130", 6379));
         jedisClusterNodes.add(new HostAndPort("192.168.0.131", 6379));
         jedisClusterNodes.add(new HostAndPort("192.168.0.132", 6379));
         jedisClusterNodes.add(new HostAndPort("192.168.0.133", 6379));
         jedisClusterNodes.add(new HostAndPort("192.168.0.134", 6379));
         jedisClusterNodes.add(new HostAndPort("192.168.0.135", 6379));
+        jedisClusterNodes.add(new HostAndPort("192.168.0.136", 6379));
         JedisCluster jedisCluster = new JedisCluster(jedisClusterNodes);
         return jedisCluster;
     }
